83ee2bb67684d12d3aa81c95fa96150bbadd13d5
imsagar21 <imsagar4506@gmail.com>
2022-11-11 07:29:42 +0530

    Second change

--- src/Components/pages/NavbarSection.js
+++ src/Components/pages/NavbarSection.js
@@ -19,8 +19,8 @@ function NavbarSection() {
                   Courses
                 </button>
                 <ul className="dropdown-menu mx-auto">
-                  <li><button className="dropdown-item">AWS</button></li>
                   <li><button className="dropdown-item">Azure</button></li>
+                  <li><button className="dropdown-item">AWS</button></li>
                   <li><button className="dropdown-item">DevOps</button></li>
                   <li><button className="dropdown-item">Azure DevOps</button></li>
                   <li><button className="dropdown-item">Salesforce</button></li>
